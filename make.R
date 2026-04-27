128e140d71cfa30a94ba13ee898a2d78d6e0b89f
Lisa Nicvert <lisa.nicvert@fondationbiodiversite.fr>
2026-04-27 10:25:41 +0200

    renamed analysis files + added call to effectsize in script 03_example_network

--- make.R
+++ make.R
@@ -16,19 +16,22 @@ thr <- 1
 
 # Run analyses ------------------------------------------------------------
 
-# Prepare data (writes data to outputs/01_clean_data)
-source(file.path(analyses_folder, "01_clean_data.R"))
+# Plot Figure 1 (writes Figure to figures/01_Fig_1_summary/)
+source(file.path(analyses_folder, "01_Fig_1_summary.R"))
+
+# Prepare data (writes data to outputs/02_clean_data)
+source(file.path(analyses_folder, "02_clean_data.R"))
 
 # Analyze network Peru1
 dataset <- "Peru1"
-quarto::quarto_render(file.path(analyses_folder, "02_example_network.qmd"),
+quarto::quarto_render(file.path(analyses_folder, "03_example_network.qmd"),
                       execute_params = list(dataset = dataset,
                                             thr = thr,
                                             colco = colco, 
                                             colli = colli))
 
 # Analyze all networks
-quarto::quarto_render(file.path(analyses_folder, "03_all_networks.qmd"),
+quarto::quarto_render(file.path(analyses_folder, "04_all_networks.qmd"),
                       execute_params = list(thr = thr, 
                                             colco = colco, 
                                             colli = colli))
@@ -37,5 +40,5 @@ quarto::quarto_render(file.path(analyses_folder, "03_all_networks.qmd"),
 # This takes a long time to run (~17 hours)
 # Change nrep to a smaller number to make tests
 nrep <- 300
-quarto::quarto_render(file.path(analyses_folder, "04_corrected_R2.qmd"),
+quarto::quarto_render(file.path(analyses_folder, "05_corrected_R2.qmd"),
                       execute_params = list(nrep = nrep))
